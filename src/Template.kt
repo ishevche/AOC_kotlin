03cd31a282a9ad62def2b47d12b68c947d9e4893
ishevche <66466185+ishevche@users.noreply.github.com>
2024-12-01 08:15:55 +0100

    Update template

--- src/Template.kt
+++ src/Template.kt
@@ -1,15 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return 1
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return 1
     }
 
     val testInput = readInput("sample")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(testInput) == 1) { part1(testInput) }
+    check(part2(testInput) == 1) { part2(testInput) }
     val input = readInput("Day__")
     part1(input).println()
     part2(input).println()
